d86bf70a1b8dce41e6c0ebc257a6de2176f49a64
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-02-02 08:04:29 +0530

    added properties for student class and corresponding methods

--- src/main/java/com/example/springdatajpa/entity/Student.java
+++ src/main/java/com/example/springdatajpa/entity/Student.java
@@ -1,4 +1,24 @@
 package com.example.springdatajpa.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Student {
+    // whatever we do in the student class will be reflected in the database because of spring.jpa.hibernate.ddl-auto=update
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
 }
