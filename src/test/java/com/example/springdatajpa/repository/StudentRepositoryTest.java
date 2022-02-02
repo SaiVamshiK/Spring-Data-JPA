78f84c595d6a138ceca9ca89299ade49737f73bb
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-02-02 13:42:47 +0530

    added a test case for saving data to db

--- src/test/java/com/example/springdatajpa/repository/StudentRepositoryTest.java
+++ src/test/java/com/example/springdatajpa/repository/StudentRepositoryTest.java
@@ -1,7 +1,26 @@
 package com.example.springdatajpa.repository;
 
+import com.example.springdatajpa.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class StudentRepositoryTest {
-
+    @Autowired
+    private StudentRepository studentRepository;
+    @Test
+    public void add(){
+        Student student = Student.builder()
+                .emailId("sai@gmail.com")
+                .firstName("sai")
+                .lastName("vamshi")
+                .guardianEmail("rahul@gmail.com")
+                .guardianMobile("8483484844")
+                .guardianName("rahul")
+                .build();
+        studentRepository.save(student);
+    }
 }
