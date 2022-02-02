7a5b15c61cb412a6215a20c5156bc424d5a69539
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-02-02 08:27:57 +0530

    using JPA annotations for making the table and column names as custom

--- src/main/java/com/example/springdatajpa/entity/Student.java
+++ src/main/java/com/example/springdatajpa/entity/Student.java
@@ -5,22 +5,35 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(name = "tbl_student")
 public class Student {
     // whatever we do in the student class will be reflected in the database because of spring.jpa.hibernate.ddl-auto=update
     @Id
+    @Column(name = "studentID")
     private Long studentId;
+    @Column(name = "firstName")
     private String firstName;
+    @Column(name = "lastName")
     private String lastName;
+    @Column(name = "emailId")
     private String emailId;
+    @Column(name = "guardianName")
     private String guardianName;
+    @Column(name = "guardianEmail")
     private String guardianEmail;
+    @Column(name = "guardianMobile")
     private String guardianMobile;
 }
+
+// @Table makes the table name as the same as we defined, in the MySql DB
+// Similarly @Column also makes the column names as the same as we defined, in the MySQl DB
