6173e32671baa6f6669bfca75ce67d7f895e66bb
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-02-02 14:21:00 +0530

    removing errors because of embedded class

--- src/main/java/com/example/springdatajpa/entity/Guardian.java
+++ src/main/java/com/example/springdatajpa/entity/Guardian.java
@@ -1,9 +1,18 @@
 package com.example.springdatajpa.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Guardian {
     @Column(name = "Name")
     private String name;
--- src/test/java/com/example/springdatajpa/repository/StudentRepositoryTest.java
+++ src/test/java/com/example/springdatajpa/repository/StudentRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.example.springdatajpa.repository;
 
+import com.example.springdatajpa.entity.Guardian;
 import com.example.springdatajpa.entity.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +16,19 @@ class StudentRepositoryTest {
     private StudentRepository studentRepository;
     @Test
     public void add(){
+        Guardian guardian = Guardian.builder()
+                .email("virat@gmail.com")
+                .mobile("8483484844")
+                .name("virat")
+                .build();
         Student student = Student.builder()
                 .emailId("dhoni@gmail.com")
                 .firstName("dhoni")
                 .lastName("ms")
-                .guardianEmail("virat@gmail.com")
-                .guardianMobile("8483484844")
-                .guardianName("virat")
+                .guardian(guardian)
+//                .guardianEmail("virat@gmail.com")
+//                .guardianMobile("8483484844")
+//                .guardianName("virat")
                 .build();
         studentRepository.save(student);
     }
