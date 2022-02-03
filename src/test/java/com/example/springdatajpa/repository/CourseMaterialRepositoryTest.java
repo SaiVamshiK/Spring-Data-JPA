6bdee8a8b02eb2383135df4baa1afa0731caa586
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-02-03 16:16:19 +0530

    removed error while storing data to db

--- src/test/java/com/example/springdatajpa/repository/CourseMaterialRepositoryTest.java
+++ src/test/java/com/example/springdatajpa/repository/CourseMaterialRepositoryTest.java
@@ -3,21 +3,23 @@ package com.example.springdatajpa.repository;
 import com.example.springdatajpa.entity.Course;
 import com.example.springdatajpa.entity.CourseMaterial;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CourseMaterialRepositoryTest {
+    @Autowired
     private CourseMaterialRepository courseMaterialRepository;
     @Test
     public void addCourseMaterial(){
         Course course = Course.builder()
-                .title("DSA")
+                .title("DAA")
                 .credits(3)
                 .build();
         CourseMaterial courseMaterial = CourseMaterial.builder()
-                .url("www.google.com")
+                .url("www.yahoo.com")
                 .course(course)
                 .build();
         courseMaterialRepository.save(courseMaterial);
