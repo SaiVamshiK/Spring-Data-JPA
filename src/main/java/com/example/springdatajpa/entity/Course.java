babc1be0c47c06a6a4cedfc2fc102eb37b486e95
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-02-03 15:37:53 +0530

    added lombok dependencies for the 2 classes

--- src/main/java/com/example/springdatajpa/entity/Course.java
+++ src/main/java/com/example/springdatajpa/entity/Course.java
@@ -1,4 +1,17 @@
 package com.example.springdatajpa.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class Course {
+
 }
--- src/main/java/com/example/springdatajpa/entity/CourseMaterial.java
+++ src/main/java/com/example/springdatajpa/entity/CourseMaterial.java
@@ -1,4 +1,16 @@
 package com.example.springdatajpa.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class CourseMaterial {
 }
