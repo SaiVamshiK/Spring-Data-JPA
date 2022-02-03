8442224a4b1b0d79ce94f89ab83d5568cc11f7b5
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-02-03 15:54:19 +0530

    minor changes]

--- src/test/java/com/example/springdatajpa/repository/CourseMaterialRepositoryTest.java
+++ src/test/java/com/example/springdatajpa/repository/CourseMaterialRepositoryTest.java
@@ -1,7 +1,10 @@
 package com.example.springdatajpa.repository;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class CourseMaterialRepositoryTest {
 
 }
