e4095d7ac6a410c7bf1869ceeae5ce9a20697e2b
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-02-03 16:10:18 +0530

    created test for saving a CourseRepo with Course ERR

--- src/test/java/com/example/springdatajpa/repository/CourseMaterialRepositoryTest.java
+++ src/test/java/com/example/springdatajpa/repository/CourseMaterialRepositoryTest.java
@@ -1,10 +1,25 @@
 package com.example.springdatajpa.repository;
 
+import com.example.springdatajpa.entity.Course;
+import com.example.springdatajpa.entity.CourseMaterial;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CourseMaterialRepositoryTest {
-
+    private CourseMaterialRepository courseMaterialRepository;
+    @Test
+    public void addCourseMaterial(){
+        Course course = Course.builder()
+                .title("DSA")
+                .credits(3)
+                .build();
+        CourseMaterial courseMaterial = CourseMaterial.builder()
+                .url("www.google.com")
+                .course(course)
+                .build();
+        courseMaterialRepository.save(courseMaterial);
+    }
 }
